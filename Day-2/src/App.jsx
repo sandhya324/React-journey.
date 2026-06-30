cc53750ea27ee1d190887bdcc45bf04643be673c
sandhya324 <sdhya3842@gmail.com>
2026-06-30 16:06:10 +0530

    my first react component

---
+++ Day-2/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'    
+// import React from 'react' : In modern React (using Vite), 
+// this line is usually not required anymore. Older React versions needed it
+//  because JSX was converted using the React object.Nowadays, 
+// Vite and newer React versions handle that automatically.
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hii there !</h1>
+      <p>I'm learning React.</p>
+    </div>
+  )
+}
+
+export default App
